61d1f84c4a39a3a5bd85fdffa557f6b9c9ac3797
Alon <769824456@qq.com>
2015-08-08 08:02:02 +0800

    change android.R.id.home

--- app/src/main/java/cn/syl/AnotherActivity.java
+++ app/src/main/java/cn/syl/AnotherActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -41,18 +42,19 @@ public class AnotherActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+
+        //这边要特别注意：是android.R.id.home不是R.id.home
+
+        if (id == android.R.id.home) {
+            finish();
+            Log.e("AnotherActivity", "home");
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
